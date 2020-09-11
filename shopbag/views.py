dbbc7a6386be7ab49df5c40e3dff2ae8a0470476
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-09-11 20:06:50 +0100

    added remove option in shopping bag, and update function, added bootstrap toasts

--- shopbag/views.py
+++ shopbag/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 
 def view_shopbag(request):
     """ View for shop bag """
 
-    return render(request,'shopbag/shopbag.html')
+    return render(request, 'shopbag/shopbag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add quantity to a product """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,6 +23,33 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-    
+        messages.success(request, f'Added {product.name} to your bag')
+
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity to a product """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_shopbag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove product """
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
