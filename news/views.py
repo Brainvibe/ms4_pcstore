a15347a79e6589ee5f60f7e2f95e90faad7aaed1
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-09-29 21:33:54 +0100

    added wireframes, changed news layout to be more responsive, refactored css, fixed button positioning in profile

--- news/views.py
+++ news/views.py
@@ -1,68 +1,26 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.db.models.functions import Lower
-from .models import Post, Category
+from .models import Post
 from .forms import PostForm
 
 
 def all_posts(request):
-    """ A view to return all blog posts """
+    """ A view to return all news posts """
 
     posts = Post.objects.all()
-    query = None
-    categories = ['new_releases', 'old_is_gold', 'news']
-    sort = None
-    direction = None
-
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                posts = posts.annotate(lower_name=Lower('title'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            posts = posts.order_by(sortkey)
-
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            posts = posts.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "Please enter a keyword to search.")
-                return redirect(reverse('news'))
-
-            queries = Q(title__icontains=query) | Q(
-                post_snippet__icontains=query) | Q(
-                post_content__icontains=query) | Q(
-                category__friendly_name__icontains=query)
-
-            posts = posts.filter(queries)
-
-    current_sorting = f'{sort}_{direction}'
-
+    categories = ['info-post', 'promo-post', 'news-post']
     context = {
         'posts': posts,
-        'search_term': query,
         'current_categories': categories,
-        'current_sorting': current_sorting,
+
     }
 
     return render(request, 'news/news.html', context)
 
 
 def post_detail(request, post_id):
-    """ A view to return the individual blog post """
+    """ A view to return the individual news post """
 
     post = get_object_or_404(Post, pk=post_id)
 
@@ -75,7 +33,7 @@ def post_detail(request, post_id):
 
 @login_required
 def add_post(request):
-    """ Add a post to the blog """
+    """ Add a news post """
 
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
@@ -86,7 +44,7 @@ def add_post(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added post!')
-            return redirect(reverse('add_post'))
+            return redirect(reverse('news'))
         else:
             messages.error(
                 request, 'Failed to add post. Please ensure the form is valid.')
@@ -103,7 +61,7 @@ def add_post(request):
 
 @login_required
 def edit_post(request, post_id):
-    """ Edit a post in the blog """
+    """ Edit a news post """
 
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
@@ -134,7 +92,7 @@ def edit_post(request, post_id):
 
 @login_required
 def delete_post(request, post_id):
-    """ Delete a post from the blog """
+    """ Delete a news post """
 
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
